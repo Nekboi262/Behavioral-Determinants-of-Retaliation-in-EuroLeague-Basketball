e444eee4ba0bfc28a024c78d34edce01e04c5645
Nekboi262 <nektariospapoutsis@yahoo.gr>
2026-09-05 12:44:28 +0300

    Add package dependencies

---
+++ R/00_packages.R
@@ -0,0 +1,16 @@
+# ============================================================
+# Packages
+# Behavioral Determinants of Retaliation in EuroLeague Basketball
+# ============================================================
+
+library(euroleaguer)
+library(haven)
+library(dplyr)
+library(ggplot2)
+library(stringr)
+library(lubridate)
+library(janitor)
+library(countrycode)
+library(modelsummary)
+library(sandwich)
+library(effects)
